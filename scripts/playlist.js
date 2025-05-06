296e3dbbb74bda7b5759dcb4bef900730b2a7927
Faisal <pros3sa@gmail.com>
2025-05-06 13:19:30 +0000

    file: add new audio to playlist

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -2,110 +2,73 @@
 export const playlist = [
     {
       id: 1,
-      title: "Isra",
+      title: "Najm",
       artist: "husarey",
-      src: "./audio/001.mp3",
-      cover: "./assets/images/roomdesign4.jpg",
-      duration: "3:33"
+      src: "./audio/053.mp3",
+      cover: "./assets/images/husarey.WEBP",
+      duration: "8:39"
     },
 
     {
         id: 2,
-        title: "Furkan",
-        artist: "Sheikh Sudais",
-        src: "./audio/017.mp3",
-        cover: "./assets/images/image.png",
-        duration: "78:33"
+        title: "Faatiha",
+        artist: "Haadi Toure",
+        src: "./audio/001.mp3",
+        cover: "./assets/images/toure.JPG",
+        duration: "0:46"
       },
       {
-        id: 1,
-        title: "Isra",
-        artist: "husarey",
-        src: "./audio/001.mp3",
-        cover: "./assets/images/roomdesign4.jpg",
-        duration: "3:33"
+        id: 3,
+        title :"Annaas",
+        artist: "Ibra Walk",
+        src: "./audio/114.mp3",
+        cover: "./assets/images/walk.JPG",
+        duration: "00:25"
+      },
+      {
+        id: 4,
+        title :"Falak",
+        artist: "Ibra Walk",
+        src: "./audio/113.mp3",
+        cover: "./assets/images/walk.JPG",
+        duration: "00:25"
+      },
+
+      {
+        id: 5,
+        title :"Ikhlas",
+        artist: "Ibra Walk",
+        src: "./audio/112.mp3",
+        cover: "./assets/images/walk.JPG",
+        duration: "00:18"
+      },
+
+      {
+        id: 6,
+        title :"Masad",
+        artist: "Ibra Walk",
+        src: "./audio/111.mp3",
+        cover: "./assets/images/walk.JPG",
+        duration: "00:25"
+      },
+
+      {
+        id: 7,
+        title :"Nasr",
+        artist: "Ibra Walk",
+        src: "./audio/110.mp3",
+        cover: "./assets/images/walk.JPG",
+        duration: "00:25"
+      },
+      {
+        id: 8,
+        title :"Kafirun",
+        artist: "Ibra Walk",
+        src: "./audio/109.mp3",
+        cover: "./assets/images/walk.JPG",
+        duration: "00:25"
       },
   
-      {
-          id: 2,
-          title: "Furkan",
-          artist: "Sheikh Sudais",
-          src: "./audio/017.mp3",
-          cover: "./assets/images/roomdesign4.jpg",
-          duration: "78:33"
-        },
-
-        {
-          id: 1,
-          title: "Isra",
-          artist: "husarey",
-          src: "./audio/001.mp3",
-          cover: "./assets/images/roomdesign4.jpg",
-          duration: "3:33"
-        },
-    
-        {
-            id: 2,
-            title: "Furkan",
-            artist: "Sheikh Sudais",
-            src: "./audio/017.mp3",
-            cover: "./assets/images/roomdesign4.jpg",
-            duration: "78:33"
-          },
-
-          {
-            id: 1,
-            title: "Isra",
-            artist: "husarey",
-            src: "./audio/001.mp3",
-            cover: "./assets/images/roomdesign4.jpg",
-            duration: "3:33"
-          },
-      
-          {
-              id: 2,
-              title: "Furkan",
-              artist: "Sheikh Sudais",
-              src: "./audio/017.mp3",
-              cover: "./assets/images/roomdesign4.jpg",
-              duration: "78:33"
-            },
-
-            {
-              id: 1,
-              title: "Isra",
-              artist: "husarey",
-              src: "./audio/001.mp3",
-              cover: "./assets/images/roomdesign4.jpg",
-              duration: "3:33"
-            },
-        
-            {
-                id: 2,
-                title: "Furkan",
-                artist: "Sheikh Sudais",
-                src: "./audio/017.mp3",
-                cover: "./assets/images/roomdesign4.jpg",
-                duration: "78:33"
-              },
-
-              {
-                id: 1,
-                title: "Isra",
-                artist: "husarey",
-                src: "./audio/001.mp3",
-                cover: "./assets/images/roomdesign4.jpg",
-                duration: "3:33"
-              },
-          
-              {
-                  id: 2,
-                  title: "Furkan",
-                  artist: "Sheikh Sudais",
-                  src: "./audio/017.mp3",
-                  cover: "./assets/images/roomdesign4.jpg",
-                  duration: "78:33"
-                },
       
     
   ];
